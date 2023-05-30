9c115a4a2a0eb3d90e763ca2093ae9fdd4ed4ebe
Mahdi <test@gmail.com>
2023-05-30 23:44:18 +0330

    add purge to edit post and add comment

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -1,31 +1,53 @@
 async function commentFormHandler(event) {
-    event.preventDefault();
-  
-    const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
-  
-    const post_id = window.location.toString().split('/')[
-      window.location.toString().split('/').length - 1
-    ];
-  
-    // if there is a comment -- preventing from users submitting empty comments 
-    if (comment_text) {
-        const response = await fetch('/api/comments', {
-          method: 'POST',
-          body: JSON.stringify({
-            post_id,
-            comment_text
-          }),
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        });
-      
-        if (response.ok) {
-          document.location.reload();
-        } else {
-          alert(response.statusText);
-        }
-      }
-  }
-  
-  document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
+	event.preventDefault();
+
+	const comment_text = document
+		.querySelector('textarea[name="comment-body"]')
+		.value.trim();
+
+	const post_id = window.location.toString().split("/")[
+		window.location.toString().split("/").length - 1
+	];
+
+	// if there is a comment -- preventing from users submitting empty comments
+	if (comment_text) {
+		const response = await fetch("/api/comments", {
+			method: "POST",
+			body: JSON.stringify({
+				post_id,
+				comment_text,
+			}),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+
+		const purgePost = await fetch(
+			"https://napi.arvancloud.ir/cdn/4.0/domains/ebnekhodadad.ir/caching/purge",
+			{
+				method: "POST",
+				body: JSON.stringify({
+					purge: "individual",
+					purge_urls: [`https://www.ebnekhodadad.ir/post/${post_id}`],
+				}),
+				headers: {
+					"Content-Type": "application/json",
+					Authorization:
+						"apikey e0164ca6-006b-5d85-9dd3-3c2ced76256b",
+				},
+			}
+		);
+		console.log(await purgePost.json());
+		console.log(purgePost.ok);
+
+		if (response.ok && purgePost.ok) {
+			document.location.reload();
+		} else {
+			alert(response.statusText);
+		}
+	}
+}
+
+document
+	.querySelector(".comment-form")
+	.addEventListener("submit", commentFormHandler);
--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,29 +1,49 @@
 async function editFormHandler(event) {
-    event.preventDefault();
-  
-    const title = document.querySelector('input[name="post-title"]').value;
-    const post_text = document.querySelector('textarea[name="post-text"]').value;
-    const id = window.location.toString().split('/')[
-        window.location.toString().split('/').length - 1
-      ];
+	event.preventDefault();
 
-    const response = await fetch(`/api/posts/${id}`, {
-        method: 'PUT',
-        body: JSON.stringify({
-            title,
-            post_text
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-      });
-      
-      if (response.ok) {
-        document.location.replace('/dashboard/');
-      } else {
-        alert(response.statusText);
-      }
-  
+	const title = document.querySelector('input[name="post-title"]').value;
+	const post_text = document.querySelector(
+		'textarea[name="post-text"]'
+	).value;
+	const id = window.location.toString().split("/")[
+		window.location.toString().split("/").length - 1
+	];
+
+	const purgePost = await fetch(
+		"https://napi.arvancloud.ir/cdn/4.0/domains/ebnekhodadad.ir/caching/purge",
+		{
+			method: "POST",
+			body: JSON.stringify({
+				purge: "individual",
+				purge_urls: [`https://www.ebnekhodadad.ir/post/${id}`],
+			}),
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: "apikey e0164ca6-006b-5d85-9dd3-3c2ced76256b",
+			},
+		}
+	);
+	console.log(await purgePost.json());
+	console.log(purgePost.ok);
+
+	const response = await fetch(`/api/posts/${id}`, {
+		method: "PUT",
+		body: JSON.stringify({
+			title,
+			post_text,
+		}),
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
+
+	if (response.ok && purgePost.ok) {
+		document.location.replace("/dashboard/");
+	} else {
+		alert(response.statusText);
+	}
 }
 
-document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
+document
+	.querySelector(".edit-post-form")
+	.addEventListener("submit", editFormHandler);
